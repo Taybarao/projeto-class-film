72cb022fcb2b2f5f05b1ae4aa8ac80d7522bee67
Tay Bar;ão <130027098+Taybarao@users.noreply.github.com>
2023-10-26 18:51:40 -0300

    Add files via upload

---
+++ galeria.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({reset: true });
+sr.reveal('.bt-link', {duration: 5000 });
+
+sr.reveal('.efeito-titulo', {duration: 1000  });
+sr.reveal('.img-prev', {duration: 4000  });
+sr.reveal('.video-galeria', {duration: 2000  });
+sr.reveal('.efeito-scroll', {duration: 4000  });
